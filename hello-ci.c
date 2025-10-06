91f700b710f8199b3095ea686284d93f4098be47
huangxbbj-debug <huangxb.bj@gmail.com>
2025-10-06 14:11:02 +0200

    Update hello-ci.c

--- hello-ci.c
+++ hello-ci.c
@@ -1,8 +1,6 @@
+// hello-ci.c
 #include <stdio.h>
-
-int add(int a, int b) {
-    return a + b;
-}
+#include "hello.h"
 
 int main(void) {
     printf("Hello, CI/CD world! 2 + 3 = %d\n", add(2, 3));
