bbb5725402769fb6835c652e4cc552d912a9a083
huangxbbj-debug <huangxb.bj@gmail.com>
2025-10-06 14:19:40 +0200

    Update hello-ci.c

--- hello-ci.c
+++ hello-ci.c
@@ -3,6 +3,8 @@
 #include "hello.h"
 
 int main(void) {
-    printf("Hello, CI/CD world! 2 + 3 = %d\n", add(2, 3));
+    printf("Hello, CI/CD world!\n");
+    printf("2 + 3 = %d\n", add(2, 3));
+    printf("5 - 3 = %d\n", sub(5, 3));
     return 0;
 }
