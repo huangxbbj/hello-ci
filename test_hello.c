d79abb4227734cfb93fb0b3ac217c5a9a840bd1b
huangxbbj <huangxb.bj@gmail.com>
2025-10-08 13:50:01 +0200

    Update test_hello.c

--- test_hello.c
+++ test_hello.c
@@ -1,19 +1,15 @@
 #include "unity/unity.h"
-#include "stdio.h"
-#include "hello.h"   // contains prototypes for add() and sub()
-
+#include "hello.h"   // declare add() and sub()
 
 void setUp(void) {}
 void tearDown(void) {}
 
 void test_add(void) {
-    TEST_ASSERT_EQUAL_INT(5, add(2, 3));
-    TEST_ASSERT_EQUAL_INT(0, add(-1, 1));
+    TEST_ASSERT_EQUAL_INT(5, add(2,3));
 }
 
 void test_sub(void) {
-    TEST_ASSERT_EQUAL_INT(2, sub(5, 3));
-    TEST_ASSERT_EQUAL_INT(-2, sub(-1, 1));
+    TEST_ASSERT_EQUAL_INT(2, sub(5,3));
 }
 
 int main(void) {
