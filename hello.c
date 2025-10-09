7822b2a2e525a27744e6377e522e85506869cbad
huangxbbj <huangxb.bj@gmail.com>
2025-10-09 09:44:20 +0200

    Update hello.c

--- hello.c
+++ hello.c
@@ -8,3 +8,15 @@ int add(int a, int b) {
 int sub(int a, int b) {
     return a - b;
 }
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+int div(int a, int b) {
+    if (b == 0) {
+        // Handle divide-by-zero safely
+        return 0;
+    }
+    return a / b;
+}
